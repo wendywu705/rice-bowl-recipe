4f3c929e6e986736e7eee91504d290167a95c78e
Wendy Wu <wwa86@sfu.ca>
2021-07-13 07:00:17 +0000

    Set up linting and update README files

--- client/src/components/Header/Title.js
+++ client/src/components/Header/Title.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Title.css';
+// import logo from './logo.png';
+import chopsticks from './chopsticks.png';
 
 function Title() {
     return (
         <div className="Title">
+            {/*<img src={logo} alt='logo' height="30" color="white"/>*/}
             <h1>RICE BOWL</h1>
+            <img src={chopsticks} alt='chopsticks' height="34"/>
         </div>
     )
 }

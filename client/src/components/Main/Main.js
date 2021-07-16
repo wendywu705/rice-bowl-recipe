655800d371b44c32fddf05809947d2fb141fb632
Jaddie Tan <72632964+jdtan@users.noreply.github.com>
2021-07-16 15:22:25 -0700

    set up single recipe template

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from '../../pages/Home/Home';
 import NewRecipe from '../../pages/NewRecipe';
+import SingleRecipe from '../SingleRecipe/Single';
 
 const Main = () => {
     return (
         <Switch> {}
             <Route exact path='/' component={Home}/>
             <Route exact path='/new_recipe' component={NewRecipe}/>
+            <Route exact path='/single' component={SingleRecipe} />
         </Switch>
     );
 }
---
+++ client/src/components/SingleRecipe/Single.js
@@ -0,0 +1,17 @@
+import { React } from 'react';
+
+import { Divider } from 'antd';
+
+const SingleRecipe = () => {
+    return (
+        <div>
+            <h1>
+                Recipe Name
+            </h1>
+            <Divider />
+            Single Recipe goes here
+        </div>
+    );
+};
+
+export default SingleRecipe;

88fdfcc8626de4a4e7a5555192a16ed559388f24
Thomas Min <thomas_min@sfu.ca>
2021-07-15 22:09:16 -0700

    Connect sider to the main layout pages

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -4,18 +4,19 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../../pages/Home/Home';
 import NewRecipe from '../../pages/NewRecipe';
 import PageLayout from '../Layout/Layout';
+import SiderBar from '../Layout/Sider';
 
 const Main = () => {
-    return (
-        <div>
-            <Switch> {}
-                <Route exact path='/' component={Home}/>
-                <Route exact path='/new_recipe' component={NewRecipe}/>
-                <Route exact path='/side' component={PageLayout} />
-            </Switch>
-            
-        </div>
-    );
-}
+  return (
+    <div>
+      <SiderBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/new_recipe" component={NewRecipe} />
+        <Route exact path="/side" component={PageLayout} />
+      </Switch>
+    </div>
+  );
+};
 
 export default Main;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,27 +1,23 @@
 import Title from '../../components/Header/Title';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
-    return (
-        <div>
-            <Title/>
-            <h1 className='nav'>Navigation</h1>
+  return (
+    <div>
+      <Title />
+      <h1 className="nav">Navigation</h1>
 
-            <div className='buttons'>
-                <Link to="/">
-                    <button>
-                        Home
-                    </button>
-                </Link>
-                <Link to="/new_recipe">
-                    <button>
-                        New Recipe
-                    </button>
-                </Link>
-            </div>
-        </div>
-    );
+      <div className="buttons">
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+        <Link to="/new_recipe">
+          <button>New Recipe</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;

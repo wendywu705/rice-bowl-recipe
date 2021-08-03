fbf303cb4fa86bf01bff915cfcd526f1eba34a5a
Christina <christinazxlu@gmail.com>
2021-08-03 16:07:30 -0700

    Able the sort button work for both browse and home page

--- client/src/components/Browse/Browse.js
+++ client/src/components/Browse/Browse.js
@@ -1,5 +1,5 @@
 import './Browse.css';
-import {Col, Row} from 'antd';
+import {Col, Rate, Row} from 'antd';
 import {ButtonDropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import React, {Component} from 'react';
@@ -14,6 +14,8 @@ class Browse extends Component {
             dropdownOpenPublic: false,
         };
         this.togglePublic = this.togglePublic.bind(this);
+        this.sortByRate = this.sortByRate.bind(this);
+        this.sortByPrepTime = this.sortByPrepTime.bind(this);
     }
 
     togglePublic() {
@@ -48,6 +50,20 @@ class Browse extends Component {
         }
     };
 
+    sortByRate(){
+        const sortRate = [].concat(this.state.public)
+            .sort((a,b) => b.meta.rating - a.meta.rating);
+        this.setState( {public: sortRate} );
+        console.log('sort by rating', sortRate);
+    }
+
+    sortByPrepTime(){
+        const sortPrep = [].concat(this.state.public)
+            .sort((a,b) => b.time.prepHour*60+b.time.prepMin - a.time.prepHour*60+a.time.prepMin);
+        this.setState( {public: sortPrep} );
+        console.log('sort by prep time',sortPrep);
+    }
+
     render() {
         const style = {
             background: '#6495ED',
@@ -78,10 +94,8 @@ class Browse extends Component {
                             Sort by
                         </DropdownToggle>
                         <DropdownMenu>
-                            <DropdownItem>Rate</DropdownItem>
-                            <DropdownItem>Prep Time</DropdownItem>
-                            <DropdownItem>Total Time</DropdownItem>
-                            <DropdownItem>Serving Size</DropdownItem>
+                            <DropdownItem onClick={this.sortByRate}>Rate</DropdownItem>
+                            <DropdownItem onClick={this.sortByPrepTime}>Prep Time</DropdownItem>
                         </DropdownMenu>
                     </ButtonDropdown>
                 </div>
@@ -90,7 +104,7 @@ class Browse extends Component {
                 <div className="recipe-card">
                     <Row gutter={[10, 25]}>
                         {this.state.public.map((res) => (
-                            <Col className="recipe-row" span={6}>
+                            <Col className="recipe-row" span={6} key={res.recipeId}>
                                 <div style={style}>
                                     <Link to= {`recipe/${res.recipeId}`}>
                                         <div>
